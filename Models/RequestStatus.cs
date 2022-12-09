4b6da724fdbb2fa6d43a16639baec3cc7d903ef2
halvipan <hal.vipan@sky.uk>
2022-12-09 11:40:29 +0000

    HIPPO6: add request status model

---
+++ Models/RequestStatus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace hippolidays.Models
+{
+	public class RequestStatus
+	{
+        public int ID { get; set; }
+
+        [Required]
+        public Request Request { get; set; }
+        public int RequestID { get; set; }
+
+        [Required]
+        public User User { get; set; }
+        public int UserID { get; set; }
+
+        [Required]
+        public string status { get; set; }
+
+        [Required]
+        public string reason { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ActionDate { get; set; }
+
+    }
+}
