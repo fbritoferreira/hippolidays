7d9356e2537d3c859bb5f4b2c0234cdfd408b6e4
halvipan <hal.vipan@sky.uk>
2022-12-16 14:36:40 +0000

    HIPPO11: Add Calendar base css and rows

--- Pages/Calendar.cshtml.cs
+++ Pages/Calendar.cshtml.cs
@@ -14,19 +14,27 @@ namespace hippolidays.Pages
         public int BlockOfToday;
         public int BlocksToShow;
 
+        public int LastDateOfCurrentMonth;
+        public int LastDateOfPreviousMonth;
+
         public void OnGet()
         {
             DateTime today = DateTime.Today;
             
             int daysInMonth = DateTime.DaysInMonth(today.Year, today.Month);
             int dayOfFirstOfMonth = (int)today.Date.AddDays(1 - today.Day).DayOfWeek;
+            
             int dayOfLastOfMonth = (int)today.Date.AddDays(daysInMonth - today.Day).DayOfWeek;
-
+            dayOfFirstOfMonth = dayOfFirstOfMonth == 0 ? 7 : dayOfFirstOfMonth;
             this.BlocksToShow = daysInMonth + dayOfFirstOfMonth - 1 + 7 - dayOfLastOfMonth;
 
             this.BlockOfFirstOfMonth = dayOfFirstOfMonth;
             this.BlockOfLastOfMonth = dayOfFirstOfMonth + daysInMonth - 1;
             this.BlockOfToday = today.Day + dayOfFirstOfMonth - 1;
+
+            this.LastDateOfCurrentMonth = daysInMonth;
+            DateTime previousMonth = today.Date.AddMonths(-1);
+            this.LastDateOfPreviousMonth = DateTime.DaysInMonth(previousMonth.Year, previousMonth.Month);
         }
     }
 }
