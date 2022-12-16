bef291dad6ad7cffde63d988b605ff5c9397392b
halvipan <hal.vipan@sky.uk>
2022-12-16 10:40:39 +0000

    HIPPO11: add core for calendar

---
+++ Pages/Calendar.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hippolidays.Pages
+{
+	public class CalendarModel : PageModel
+    {
+        public int BlockOfFirstOfMonth;
+        public int BlockOfLastOfMonth;
+        public int BlockOfToday;
+        public int BlocksToShow;
+
+        public void OnGet()
+        {
+            DateTime today = DateTime.Today;
+            
+            int daysInMonth = DateTime.DaysInMonth(today.Year, today.Month);
+            int dayOfFirstOfMonth = (int)today.Date.AddDays(1 - today.Day).DayOfWeek;
+            int dayOfLastOfMonth = (int)today.Date.AddDays(daysInMonth - today.Day).DayOfWeek;
+
+            this.BlocksToShow = daysInMonth + dayOfFirstOfMonth - 1 + 7 - dayOfLastOfMonth;
+
+            this.BlockOfFirstOfMonth = dayOfFirstOfMonth;
+            this.BlockOfLastOfMonth = dayOfFirstOfMonth + daysInMonth - 1;
+            this.BlockOfToday = today.Day + dayOfFirstOfMonth - 1;
+        }
+    }
+}
