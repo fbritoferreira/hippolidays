77475c10bf816f39809813d285dcf61a70b4017d
Filipe Brito Ferreira <me@fbritoferreira.com>
2023-01-06 12:14:22 +0000

    feat(models): add request and request type models

---
+++ Models/Request.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using hippolidays.Areas.Identity.Pages.Account;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace hippolidays.Models
+{
+    public class Request
+
+    {
+        public int request_id { get; set; }
+
+        public IdentityUser user_id { get; set; }
+        
+        public RequestType request_type_id { get; set; }
+        
+        [DataType(DataType.Date)]
+        public DateTime start_date { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime end_date { get; set; }
+
+    }
+}
+
