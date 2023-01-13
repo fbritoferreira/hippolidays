ae115745fed501db9862f69736effe9babf340de
halvipan <hal.vipan@sky.uk>
2023-01-13 10:51:06 +0000

    HIPPO6: add request status model with migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -18,4 +18,5 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 
     public DbSet<hippolidays.Models.Request> Request { get; set; } = default!;
     public DbSet<hippolidays.Models.RequestType> RequestType { get; set; } = default!;
+    public DbSet<hippolidays.Models.RequestType> RequestStatus { get; set; } = default!;
 }
